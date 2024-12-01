ed3b36f1d87c036eea6a163ef62b18bf1530ee17
Yuryi Fedchanka <yuryi.fedchanka@inkbook.ltd>
2024-12-01 10:56:42 +0100

    Day01 solution. O(N * log(N)) complexity.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,57 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (aList, bList) = input.map { line ->
+            line.split("   ")
+                .map(String::toInt)
+        }
+            .associate { it[0] to it[1] }
+            .let { placesMap ->
+                placesMap.keys to placesMap.values
+            }
+
+        return aList.sorted().zip(bList.sorted())
+            .sumOf { (a, b) ->
+                abs(a - b)
+            }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (aList, bList) = input.map { line ->
+            line.split("   ")
+                .map(String::toInt)
+        }
+            .associate { it[0] to it[1] }
+            .let { placesMap ->
+                placesMap.keys.distinct().sorted() to
+                        placesMap.values.sorted()
+            }
+
+        var bPointer = 0
+        return aList.sumOf { location ->
+            if (bPointer >= bList.size) 0
+            else {
+                var appearances = 0
+                while (location > bList.getOrElse(bPointer) { Int.MAX_VALUE }) {
+                    bPointer++
+                }
+
+                while (location == bList.getOrNull(bPointer)) {
+                    appearances++
+                    bPointer++
+                }
+
+                appearances * location
+            }
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01")
+    check(part1(readInput("Day01")) == 3714264)
+
+    check(part2(testInput) == 18805872)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
